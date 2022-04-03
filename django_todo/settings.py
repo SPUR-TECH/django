b73ffab5c11441aa6bbffbf144488e3a81b48ff3
Scott Atkinson <scott.atkin79@gmail.com>
2022-04-03 09:21:14 +0000

    change title

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k#y^6(9t_jjnm6(nj=(y9+=ac7j%_g8ii=u=jun)#&g)kv*19l'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-k#y^6(9t_jjnm6(nj=(y9+=ac7j%_g8ii=u=jun)#&g)kv*19l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.environ.get('things-todo-app-django')]
 
 
 # Application definition
@@ -83,8 +83,10 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # }
 
 DATABASES = {
-   'default': dj_database_url.parse('postgres://vdaxmgrqloanze:83568f5590695061f3a000e68f1eb45b681598aa4f124996b25afff480e414ac@ec2-176-34-211-0.eu-west-1.compute.amazonaws.com:5432/d3o5fttngqjt4n')
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
+
+
 
 
 
