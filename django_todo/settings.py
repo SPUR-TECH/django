d944292ac10bd3b7c8277191fc1931a6a2f58c87
Scott Atkinson <scott.atkin79@gmail.com>
2022-04-03 08:54:28 +0000

    set for deployment

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,12 +75,17 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': dj_database_url.parse('postgres://vdaxmgrqloanze:83568f5590695061f3a000e68f1eb45b681598aa4f124996b25afff480e414ac@ec2-176-34-211-0.eu-west-1.compute.amazonaws.com:5432/d3o5fttngqjt4n')
 }
+
 
 
 # Password validation
